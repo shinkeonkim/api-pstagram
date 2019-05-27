b1a818de3b453a5ca09d23484b919a2f82987d2c
shinkeonkim <singun11@naver.com>
2019-05-27 13:55:49 +0900

    html 태그 삭제, resources.html 초기화

--- pstagram/api/profile/login.php
+++ pstagram/api/profile/login.php
@@ -1,88 +1,81 @@
-<!DOCTYPE html>
-<html>
-    <head> </head>
-    
-    <body> 
-        <?php
-            $flag = 1;
-            $error_number=-1;
-            $code = "";
-            $error_msg ="";
+<?php
+    $flag = 1;
+    $error_number=-1;
+    $code = "";
+    $error_msg ="";
 
-            $conn = mysqli_connect('localhost','root','skyjPstagram','pstagram');
-            $email=$_POST['email'];
-            $password=$_POST['password'];
-            $hash=hash("sha1",$password);
-            $sql="SELECT * from `user` where email= '{$email}';";
-            $result = mysqli_query($conn,$sql);
+    $conn = mysqli_connect('localhost','root','skyjPstagram','pstagram');
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $hash=hash("sha1",$password);
+    $sql="SELECT * from `user` where email= '{$email}';";
+    $result = mysqli_query($conn,$sql);
 
-            if(!$result)
+    if(!$result)
+    {
+        $flag = 0;
+        $error_number = 0;
+        // echo mysqli_error($conn);
+    }
+    else 
+    {
+        $row = mysqli_fetch_array($result);
+        $user_id=$row['user_id'];
+        $username=$row['username'];
+        $email=$row['email'];
+        $created_at=$row['created_at'];
+        $profile_url=$row['profile_url'];
+        
+        if($row['password'] == $hash)
+        {    
+            $flag=1;   
+            $code="success";
+        }
+        else 
+        {
+            $flag=0;
+            if(isset($row['username']))
             {
-                $flag = 0;
-                $error_number = 0;
-                // echo mysqli_error($conn);
+                $error_number=1;
             }
-            else 
+            else
             {
-                $row = mysqli_fetch_array($result);
-                $user_id=$row['user_id'];
-                $username=$row['username'];
-                $email=$row['email'];
-                $created_at=$row['created_at'];
-                $profile_url=$row['profile_url'];
-                
-                if($row['password'] == $hash)
-                {    
-                    $flag=1;   
-                    $code="success";
-                }
-                else 
-                {
-                    $flag=0;
-                    if(isset($row['username']))
-                    {
-                        $error_number=1;
-                    }
-                    else
-                    {
-                        $error_number=2;
-                    }
-                }
+                $error_number=2;
             }
-            if($flag == 1)
-            {
-                $data = array(
-                    'code' => $code,
-                    'user_id' => $user_id,
-                    'email' => $email,
-                    'username' => $username,
-                    'profile_url' => $profile_url,
-                    'created_at' => $created_at
-                );
-                echo json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-            else if($flag == 0)
-            {
-                $code = "error";
-                if($error_number == 0)
-                {
-                    $error_msg = "서버 오류가 발생했습니다. 관리자에게 문의해주세요.";
-                }
-                else if($error_number == 1)
-                {
-                    $error_msg = "존재하지 않는 사용자/이메일입니다.";
-                }
-                else if($error_number ==2)
-                {
-                    $error_msg = "비밀번호가 잘못 되었습니다.";
-                }
-                $data = array(
-                    'code' => $code,
-                    'msg' => $error_msg
-                );  
-                echo json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-            mysqli_close($conn); 
-        ?>
-    </body>
-</html>
+        }
+    }
+    if($flag == 1)
+    {
+        $data = array(
+            'code' => $code,
+            'user_id' => $user_id,
+            'email' => $email,
+            'username' => $username,
+            'profile_url' => $profile_url,
+            'created_at' => $created_at
+        );
+        echo json_encode($data,JSON_UNESCAPED_UNICODE);
+    }
+    else if($flag == 0)
+    {
+        $code = "error";
+        if($error_number == 0)
+        {
+            $error_msg = "서버 오류가 발생했습니다. 관리자에게 문의해주세요.";
+        }
+        else if($error_number == 1)
+        {
+            $error_msg = "존재하지 않는 사용자/이메일입니다.";
+        }
+        else if($error_number ==2)
+        {
+            $error_msg = "비밀번호가 잘못 되었습니다.";
+        }
+        $data = array(
+            'code' => $code,
+            'msg' => $error_msg
+        );  
+        echo json_encode($data,JSON_UNESCAPED_UNICODE);
+    }
+    mysqli_close($conn); 
+?>
--- pstagram/api/profile/register.php
+++ pstagram/api/profile/register.php
@@ -1,84 +1,74 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-    </head>
+<?php
+    
+    $flag = 1;
+    $code = "";
+    $error_number = -1;
 
-    <body> 
-        <?php
-            
-            $flag = 1;
-            $code = "error";
-            $error_number = -1;
+    $conn = mysqli_connect('localhost','root','skyjPstagram','pstagram');
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    
+    $sql = "SELECT * FROM `user` WHERE email in ('{$email}');";
+    $result = mysqli_query($conn,$sql);
+    $row= mysqli_fetch_array($result);
 
-            $conn = mysqli_connect('localhost','root','skyjPstagram','pstagram');
-            //$arr=$_POST['register'];
-            $username = $_POST['username'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            
-            $sql = "SELECT * FROM `user` WHERE email in ('{$email}');";
-            $result = mysqli_query($conn,$sql);
-            $row= mysqli_fetch_array($result);
+    if(isset($row['email']))
+    {   
+        $flag=0;
+        $error_number = 0;
+    } 
+    else 
+    {
+        $hash = hash("sha1",$password);
+        $profile_url = "localhost/pstagram/img/resources/avatar.png";
+        $created_at = date("Y-m-d H:i:s");
+        $sql = "INSERT INTO `user` (`email`, `username`,`password`,`profile_url`,`created_at`) VALUES ('$email' , '$username','$hash','$profile_url','$created_at')";
+        $result = mysqli_query($conn,$sql);
+        
+        if(!$result)
+        {
+            $flag=0;
+            $error_number=1;
+            echo mysqli_error($conn); //mysqli 쿼리문 에러 보기
+        } 
+    }
 
-            if(isset($row['email']))
-            {   
-                $flag=0;
-                $error_number = 0;
-            } 
-            else 
-            {
-                $hash = hash("sha1",$password);
-                $profile_url = "localhost/pstagram/img/resources/avatar.png";
-                $created_at = date("Y-m-d H:i:s");
-                $sql = "INSERT INTO `user` (`email`, `username`,`password`,`profile_url`,`created_at`) VALUES ('$email' , '$username','$hash','$profile_url','$created_at')";
-                $result = mysqli_query($conn,$sql);
-                
-                if(!$result)
-                {
-                    $flag=0;
-                    $error_number=1;
-                    //echo mysqli_error($conn); //mysqli 쿼리문 에러 보기
-                } 
-            }
+    if($flag == 0)
+    {
+        $error_msg = "error";
+        if($error_number == 0)
+        {
+            $error_msg="같은 이메일이 존재합니다.";
+        }
+        else if($error_number == 1)
+        {
+            $error_msg="서버 오류가 발생했습니다. 관리자에게 문의해주세요.";
+        }
+        $data = array(
+            'code' => $code,
+            'msg' => $error_msg
+        );
+        echo json_encode($data,JSON_UNESCAPED_UNICODE);
+    }
+    else 
+    {
+        $code = "success";
+        $success_msg = "회원가입이 완료되었습니다.";
+        $sql = "SELECT * FROM `user` WHERE username in ('{$username}');";
+        $result = mysqli_query($conn,$sql);
+        $row= mysqli_fetch_array($result);
+        
+        $user_id = $row['user_id'];
 
-            if($flag == 0)
-            {
-                $error_msg = "error";
-                if($error_number == 0)
-                {
-                    $error_msg="같은 이메일이 존재합니다.";
-                }
-                else if($error_number == 1)
-                {
-                    $error_msg="서버 오류가 발생했습니다. 관리자에게 문의해주세요.";
-                }
-                $data = array(
-                    'code' => $code,
-                    'msg' => $error_msg
-                );
-                echo json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-            else 
-            {
-                $code = "success";
-                $success_msg = "회원가입이 완료되었습니다.";
-                $sql = "SELECT * FROM `user` WHERE username in ('{$username}');";
-                $result = mysqli_query($conn,$sql);
-                $row= mysqli_fetch_array($result);
-                
-                $user_id = $row['user_id'];
+        $data = array(
+            'code' => $code,
+            'msg' => $success_msg,
+            'username' => $username,
+            'user_id' => $user_id
+        );
+        echo json_encode($data,JSON_UNESCAPED_UNICODE);
+    }
 
-                $data = array(
-                    'code' => $code,
-                    'msg' => $success_msg,
-                    'username' => $username,
-                    'user_id' => $user_id
-                );
-                echo json_encode($data,JSON_UNESCAPED_UNICODE);
-            }
-
-            mysqli_close($conn);
-        ?>
-    </body>
-</html>
+    mysqli_close($conn);
+?>
