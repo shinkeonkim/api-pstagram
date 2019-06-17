f028bae7c8ea95426aeebfc4daadf840fc8272d9
shinkeonkim <singun11@naver.com>
2019-06-17 14:11:00 +0900

    안드로이드와 통신 맞추기 위해 조정

--- pstagram/api/review/review_upload.php
+++ pstagram/api/review/review_upload.php
@@ -6,24 +6,24 @@
     $error_number=-1;
     $msg="";
 
-
+    /*
     $data = file_get_contents('php://input');
     $arr = json_decode($data,true);
     $user_id = $arr['user_id'];
     $content = $arr['content'];
     $rate = $arr['rate'];
+    */
     
-    /*
     $user_id = $_POST['user_id'];
     $content = $_POST['content'];
     $rate = $_POST['rate'];
-    */
+    
 
     $created_at = date("Y-m-d H:i:s");
 
 
-    $target_dir = "C:/Bitnami/wampstack-7.1.29-0/apache2/htdocs/pstagram/uploads/"; //설정 중요
-    $target_file = $target_dir . basename($_FILES["image_upload"]["name"]);
+    $target_dir = "C:/Bitnami/wampstack/apache2/htdocs/pstagram/uploads/"; //설정 중요
+    $target_file = $target_dir . basename($_FILES['image_upload']["name"]);
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
     $photo_url = $target_dir.hash("sha1",$created_at.$user_id).".".$imageFileType;
     
@@ -53,34 +53,34 @@
     }*/
 
     // 파일사이즈 체크
-    if ($_FILES["image_upload"]["size"] > 500000) 
+    if ($_FILES['image_upload']['size'] > 10000000)  
     {
         $error_number = 2; //파일의 사이즈가 너무 큽니다.
         $flag = 0;
     }
     // 파일 형식 허용
+    /*
     if($imageFileType != "jpg" && $imageFileType != "JPG" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" && $imageFileType != "GIF" && $imageFileType != "PNG" && $imageFileType != "JPEG") 
     {
         $error_number = 3; // 파일의 형식이 잘못되었습니다.
         $flag = 0;
     }
-
-
+    */
     // flag가 0일때(업로그가 실패한 경우)
-    if ($flag == 0) 
+    if ($flag == 0)  
     { 
         $code = "error";
-        $error_msg = "error";
+        $error_msg = "예기치 못한 오류입니다.";
         if($error_number == 1)
         {
             $error_msg="이미지가 아닙니다.";
         }
         else if($error_number == 2)
         {
-            $error_msg="이미지의 용량이 너무 큽니다.";
+            $error_msg="이미지의 용량이 너무 큽니다.".$content."*";
         }
-        else if($error_number == 1)
+        else if($error_number == 3)
         {
             $error_msg="파일의 형식이 잘못되었습니다";
         }
@@ -92,7 +92,7 @@
     } 
     else //업로드가 완료된 경우 
     {
-        if (move_uploaded_file($_FILES["image_upload"]["tmp_name"], $photo_url)) 
+        if (move_uploaded_file($_FILES['image_upload']['tmp_name'], $photo_url)) 
         {
             $sql = "INSERT INTO `review` (`user_id`, `content`,`photo_url`,`rate`,`created_at`) VALUES ('$user_id' , '$content','$img_url','$rate','$created_at')";
             $result = mysqli_query($conn,$sql);
@@ -101,7 +101,7 @@
             {
                 $data = array(
                     'code' => "error",
-                    'msg' => "업로드 과정 중, 에러가 발생하였습니다."
+                    'msg' => "업로드 과정 중, 에러가 발생하였습니다.".$user_id.$content.(string)$rate
                 );
                 echo str_replace('\\/', '/',json_encode($data,JSON_UNESCAPED_UNICODE));    
             }
@@ -118,7 +118,7 @@
         {
             $data = array(
                 'code' => "error",
-                'msg' => "업로드 과정 중, 에러가 발생하였습니다."
+                'msg' => "업로드 과정 중, 에러가 발생하였습니다.".$user_id.$content.(string)$rate."*"
             );
             echo str_replace('\\/', '/',json_encode($data,JSON_UNESCAPED_UNICODE));
         }
